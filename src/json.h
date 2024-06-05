d7de719ebc12dcd8828663ed2736f8369baa58ca
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-05 16:07:07 +0100

    Update json.h

--- src/json.h
+++ src/json.h
@@ -10,4 +10,27 @@ typedef enum json_type {
     JSON_TYPE_OBJECT
 } json_type;
 
+typedef struct json_string {
+    const char * string;
+    size_t length;
+} json_string;
+
+typedef struct json_array {
+    json_value * values;
+    size_t length;
+} json_array;
+
+typedef struct json_object {
+
+} json_object;
+
+typedef struct json_value {
+    json_type type;
+    union value {
+        bool boolean;
+        json_string string;
+        json_array array;
+    };
+} json_value;
+
 #endif
