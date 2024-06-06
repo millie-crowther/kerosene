732e00fae2ed5bb1b0e0b1a53c0f7554dc337572
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-06 12:02:45 +0100

    Update json.h

--- src/json.h
+++ src/json.h
@@ -20,8 +20,22 @@ typedef struct json_array {
     size_t length;
 } json_array;
 
-typedef struct json_object {
+typedef struct json_object_key_pair {
+    json_string key;
+    json_value * value;
+} json_object_key_pair;
 
+typedef struct json_object_bucket {
+    json_object_key_pair * key_pairs;
+    size_t count;
+} json_object_bucket;
+
+#define JSON_OBJECT_BUCKETS_COUNT 32
+
+typedef struct json_object {
+    json_object_bucket buckets[JSON_OBJECT_BUCKETS_COUNT];
+    json_value * values;
+    size_t count;
 } json_object;
 
 typedef struct json_value {
@@ -35,6 +49,5 @@ typedef struct json_value {
 
 size_t json_value_count(const json_string string);
 bool json_parse(const json_string string, json_value * output);
-
 
 #endif
