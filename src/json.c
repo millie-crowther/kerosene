d612e05061ed3e89c43c3a72c5c2aa59f412b457
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-07 14:35:34 +0100

    Update json.c

--- src/json.c
+++ src/json.c
@@ -1,11 +1,11 @@
 #include "json.h"
 
-size_t value_count(const char * string, size_t length){
+size_t value_count_upper_bound(const char * string, size_t length){
     // TODO: lower bound
     return length;
 }
 
-size_t key_pair_count(const char * string, size_t length){
+size_t key_pair_count_upper_bound(const char * string, size_t length){
     size_t count = 0;
     for (size_t i = 0; i < length; i++){
         if (string[i] == ':'){
