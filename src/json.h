38f745efb84a07c8cc0434ad5ffb966fda751efe
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-07 17:17:23 +0100

    Update json.h

--- src/json.h
+++ src/json.h
@@ -1,6 +1,8 @@
 #ifndef JSON_H
 #define JSON_H
 
+#include "string.h"
+
 typedef enum json_type {
     JSON_TYPE_NULL,
     JSON_TYPE_BOOLEAN,
@@ -10,30 +12,25 @@ typedef enum json_type {
     JSON_TYPE_OBJECT
 } json_type;
 
-typedef struct json_string {
-    const char * string;
-    size_t length;
-} json_string;
-
 typedef struct json_array {
     json_value * values;
-    size_t length;
+    uint32_t length;
 } json_array;
 
 typedef struct json_object_key_pair {
-    json_string * key;
+    string key;
     json_value * value;
 } json_object_key_pair;
 
 typedef struct json_object_bucket {
     json_object_key_pair * key_pairs;
-    size_t length;
+    uint32_t length;
 } json_object_bucket;
 
 #define JSON_OBJECT_BUCKETS_COUNT 32
 
 typedef struct json_object {
-    json_object_bucket buckets[JSON_OBJECT_BUCKETS_COUNT];
+    json_object_bucket * buckets;
 } json_object;
 
 typedef struct json_value {
@@ -41,7 +38,7 @@ typedef struct json_value {
     union {
         bool boolean;
         double number;
-        json_string string;
+        string string;
         json_array array;
         json_object object;
     };
@@ -51,12 +48,13 @@ typedef struct json_document {
     json_value * values;
     json_object_key_pair * key_pairs;
     const char * string;
+    json_object_bucket * bucket;
 
     void * data_pointer;
 } json_document;
 
-json_value * json_object_get(const json_object * object, const json_string string);
-bool json_document_parse(const json_string string, json_document * document);
+json_value * json_object_get(const json_object * object, const string string);
+bool json_document_parse(const string string, json_document * document);
 void json_document_destroy(json_document * document);
 
 #endif
