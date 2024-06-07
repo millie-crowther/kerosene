e79861a0286562313785d188351cc2641e07f9dd
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-07 14:34:50 +0100

    Update json.c

--- src/json.c
+++ src/json.c
@@ -8,7 +8,7 @@ size_t value_count(const char * string, size_t length){
 size_t key_pair_count(const char * string, size_t length){
     size_t count = 0;
     for (size_t i = 0; i < length; i++){
-        if (string[i] == ":"){
+        if (string[i] == ':'){
             count++;
         }
     }
