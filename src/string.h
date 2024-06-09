64207edad8ae0d7c49c450cd65d76428dc9dd18b
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-09 11:56:00 +0100

    Update string.h

--- src/string.h
+++ src/string.h
@@ -10,5 +10,6 @@ bool string_equals(const string a, const string b);
 bool string_starts_with(const string string, const string prefix);
 bool string_contains_character(const string string, const char character);
 string string_tail(const string string);
+char string_head(const string string);
 
 #endif
