3d281d9bba018da4fd51489d7fc5eb95defa7e54
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-09 12:59:17 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+#include "json.h"
+
 int main(){
+    if (!json_compile_regular_expressions()){
+        printf("Failed to compile regular expressions\n");
+        return -1;
+    }
     printf("hello world!");
 }
