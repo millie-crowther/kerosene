1aff65bbb9230eb4e3fba658e2dc406335b6f383
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-09 10:49:03 +0100

    Update string.h

--- src/string.h
+++ src/string.h
@@ -9,5 +9,6 @@ typedef struct string {
 bool string_equals(const string a, const string b);
 bool string_starts_with(const string string, const string prefix);
 bool string_contains_character(const string string, const char character);
+string string_tail(const string string);
 
 #endif
