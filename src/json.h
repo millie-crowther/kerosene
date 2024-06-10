630590818ac1f2f5287195127a453e4e18130c37
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-10 15:17:23 +0100

    Update json.h

--- src/json.h
+++ src/json.h
@@ -44,7 +44,7 @@ typedef struct json_value_t {
 
 typedef struct json_document_t {
     void * data_pointer;
-    json_value_t * root;
+    json_value_t root;
 } json_document_t;
 
 bool json_compile_regular_expressions();
