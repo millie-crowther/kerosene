3bc8e2680cf0096b3ea94d35af3b3fc69db9c1f9
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-10 11:48:52 +0100

    Update json.h

--- src/json.h
+++ src/json.h
@@ -3,63 +3,61 @@
 
 #include <stdint.h>
 
-#include "string.h"
-
-typedef enum json_type {
+typedef enum json_type_t {
     JSON_TYPE_NULL,
     JSON_TYPE_BOOLEAN,
     JSON_TYPE_NUMBER,
     JSON_TYPE_STRING,
     JSON_TYPE_LIST,
     JSON_TYPE_OBJECT
-} json_type;
+} json_type_t;
 
-struct json_value;
+struct json_value_t;
 
-typedef struct json_array {
-    struct json_value * values;
+typedef struct json_array_t {
+    struct json_value_t * values;
     uint32_t length;
-} json_array;
+} json_array_t;
 
-typedef struct json_object_key_pair {
-    string key;
-    struct json_value * value;
-} json_object_key_pair;
+typedef struct json_object_key_pair_t {
+    const char * string;
+    struct json_value_t * value;
+} json_object_key_pair_t;
 
-typedef struct json_object_bucket {
-    json_object_key_pair * key_pairs;
+typedef struct json_object_bucket_t {
+    json_object_key_pair_t * key_pairs;
     uint32_t length;
-} json_object_bucket;
+} json_object_bucket_t;
 
 #define JSON_OBJECT_BUCKETS_COUNT 32
 
-typedef struct json_object {
-    json_object_bucket * buckets;
-} json_object;
+typedef struct json_object_t {
+    json_object_bucket_t * buckets;
+} json_object_t;
 
-typedef struct json_value {
-    json_type type;
+typedef struct json_value_t {
+    json_type_t type;
     union {
         bool boolean;
         double number;
-        string string;
-        json_array array;
-        json_object object;
+        const char * string;
+        json_array_t array;
+        json_object_t object;
     };
-} json_value;
+} json_value_t;
 
-typedef struct json_document {
-    json_value * values;
-    json_object_key_pair * key_pairs;
+typedef struct json_document_t {
+    json_value_t * values;
+    json_object_key_pair_t * key_pairs;
     const char * string;
     json_object_bucket * bucket;
 
     void * data_pointer;
-} json_document;
+} json_document_t;
 
 bool json_compile_regular_expressions();
-json_value * json_object_get(const json_object * object, const string string);
-bool json_document_parse(const string string, json_document * document);
-void json_document_destroy(json_document * document);
+json_value_t * json_object_get(const json_object_t * object, const char * string);
+bool json_document_parse(const char * string, json_document_t * document);
+void json_document_destroy(json_document_t * document);
 
 #endif
