41ed6193d98c407fd87ee5c78e5fa04d4fc4453b
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-10 17:01:58 +0100

    Update json.c

--- src/json.c
+++ src/json.c
@@ -51,7 +51,7 @@ json_token_t * check_scope(json_token_t * tokens, uint32_t scope, json_token_t *
         }
         
         if (tokens->type == JSON_TOKEN_TYPE_OPEN_BRACE || tokens->type == JSON_TOKEN_TYPE_OPEN_BRACKET){
-            tokens = check_scope(tokens + 1, scope + 1, tokens);
+            tokens = check_scope(tokens + 1, scope + 1, tokens, end);
         } else if (tokens->type == JSON_TOKEN_TYPE_CLOSE_BRACE || tokens->type == JSON_TOKEN_TYPE_CLOSE_BRACKET){
             return tokens;
         }
