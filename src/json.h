491592db8bd3d7ac926d012673f46a73ffd48551
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-12 15:14:01 +0100

    Update json.h

--- src/json.h
+++ src/json.h
@@ -13,9 +13,10 @@ typedef enum json_type_t {
 } json_type_t;
 
 struct json_value_t;
+struct json_document_t;
 
 typedef struct json_array_t {
-    struct json_value_t * elements;
+    struct json_value_t ** elements;
     uint32_t length;
 } json_array_t;
 
@@ -26,12 +27,8 @@ typedef struct json_key_pair_t {
     struct json_key_pair_t * next;
 } json_key_pair_t;
 
-typedef struct json_hash_map_t {
-    json_key_pair_t ** buckets;
-    uint32_t length;
-} json_hash_map_t;
-
 typedef struct json_value_t {
+    struct json_document_t * document;
     json_type_t type;
     union {
         bool boolean;
@@ -45,10 +42,16 @@ typedef struct json_value_t {
 typedef struct json_document_t {
     void * data_pointer;
     json_value_t * root;
+
+    json_value_t ** arrays;
+
+    json_key_pair_t ** buckets;
+    uint32_t buckets_length;
+    json_key_pair_t * key_pairs;
 } json_document_t;
 
 bool json_compile_regular_expressions();
-json_value_t * json_object_get(const json_object_t * object, const char * string);
+json_value_t * json_object_get(const json_value_t * object, const char * string);
 bool json_document_parse(const char * string, json_document_t * document);
 void json_document_destroy(json_document_t * document);
 
