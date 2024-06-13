3d9540d9a0c722e69596b7ece9bd4a53ba71bb5f
millie-crowther <amelia.g.crowther@gmail.com>
2024-06-13 10:59:03 +0100

    Update json.c

--- src/json.c
+++ src/json.c
@@ -145,22 +145,27 @@ json_value_t * parse_json_value(json_parser_t * parser){
     } else if (token.type == JSON_TOKEN_TYPE_OPEN_BRACKET){
         result->array = (json_array_t){ .elements = parser->arrays };
         
-        while (parser->tokens->type != JSON_TOKEN_TYPE_CLOSE_BRACKET){
-            json_value_t * element = parse_json_value(parser);
-            if (element == nullptr){
-                return nullptr;
-            }
-            result->array->elements[result->array->length] = element;
-            result->array->length++;
-            parser->arrays++;
-
-            json_token_t next_token = *(parser->tokens);
-            parser->tokens++;
-            if (next_token.type == JSON_TOKEN_TYPE_CLOSE_BRACKET){
+        while (true){
+            if (parser->tokens->type == JSON_TOKEN_TYPE_CLOSE_BRACKET){
+                parser->tokens++;
                 return result;
-            } else if (next_token.type != JSON_TOKEN_TYPE_COMMA){
-                return nullptr;
-            }
+            } else {
+                json_value_t * element = parse_json_value(parser);
+                if (element == nullptr){
+                    return nullptr;
+                }
+                result->array->elements[result->array->length] = element;
+                result->array->length++;
+                parser->arrays++;
+
+                if (
+                    parser->tokens->type != JSON_TOKEN_TYPE_COMMA &&
+                    parser->tokens->type != JSON_TOKEN_TYPE_CLOSE_BRACKET
+                ){
+                    return nullptr;
+                }
+                parser->tokens++;
+            } 
         }
     } 
     
